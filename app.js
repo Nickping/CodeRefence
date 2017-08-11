9ef3d3bb4abe828419da49e5af9ad2bbe8bd92e5
JeongEuiJoon <dordong327@gmail.com>
2017-08-11 18:03:01 +0900

    REST API get/post/put/delete

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
-var myroute = require('./routes/question');
+var question = require('./routes/question');
 var app = express();
 
 // view engine setup
@@ -27,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/myroute',myroute);
+app.use('/question',question);
 // catch 404 and forward to errore handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
--- routes/question.js
+++ routes/question.js
@@ -10,7 +10,7 @@ router.get('/',(req, res)=>{
 });
 
 //route for creating questions
-router.post('/question',(req, res)=>{
+router.post('/',(req, res)=>{
     //return all the questions
 
     res.json({
@@ -18,6 +18,76 @@ router.post('/question',(req, res)=>{
         body : req.body
     });
 });
+//route for specific questions
+router.get('/:qID',(req, res)=>{
+    //return all the questions
+
+    res.json({
+        response : "You sent me a GET request ID : "+req.params.qID,
+    });
+});
+
+//POST /question/:id/answers
+//route for specific questions
+router.post('/:qID/answers',(req, res)=>{
+    //return all the questions
+
+    res.json({
+        response : "You sent me a POST request to /answers",
+        questionID : req.params.qID,
+        body: req.body
+
+    });
+});
+
+
+//PUT /questions/:qID/answers/:aID
+//Edit a specific answer
+
+router.put('/:qID/answers/:aID',(req, res)=>{
+    //return all the questions
+
+    res.json({
+        response : "You sent me a PUT request to /answers",
+        questionID : req.params.qID,
+        answerID : req.params.aID,
+        body: req.body
+
+    });
+});
+
+
+
+//DELETE /questions/:qID/answers/:aID
+//delte a specific answer
+
+router.delete('/:qID/answers/:aID',(req, res)=>{
+    //return all the questions
+
+    res.json({
+        response : "You sent me a DELETE request to /answers",
+        questionID : req.params.qID,
+        answerID : req.params.aID
+    });
+});
+
+
+
+//POST /questions/:qID/answers/:aID/vote-up
+//POST /questions/:qID/answers/:aID/vote-down
+//VOTE a specific answer
+
+router.post('/:qID/answers/:aID/vote-:dir',(req, res)=>{
+    //return all the questions
+
+    res.json({
+        response : "You sent me a POST request to /vote-"+req.params.dir,
+        questionID : req.params.qID,
+        answerID : req.params.aID,
+        vote : req.params.dir
+    });
+});
+
 
 
 module.exports = router;
