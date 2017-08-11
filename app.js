e79950ed31188c0d3c2b8c249290e72c735c5e16
JeongEuiJoon <dordong327@gmail.com>
2017-08-11 18:09:23 +0900

    error json으로 보내기

--- app.js
+++ app.js
@@ -42,7 +42,12 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  // res.render('error');
+  res.json({
+      error: {
+        message : err.message
+      }
+  });
 });
 
 module.exports = app;
